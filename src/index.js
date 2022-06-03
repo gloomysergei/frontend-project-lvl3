f722a4981c56f96d817144956d44e9977dd67908
gloomysergei <almazopal@gmail.com>
2022-06-03 20:43:49 +0300

    feature(index.html) creating a template HTML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import './style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
 import init from './init.js';
 
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './index.html',
     }),
   ],
 };
